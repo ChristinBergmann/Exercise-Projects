f29e088f4e3efc97257efc28653f0068bd3f4753
Christin Bergmann <christin.bergmann@gmx.net>
2020-05-15 16:29:06 +0200

    add fcts

--- Projekt7-speakingWhale/main.js
+++ Projekt7-speakingWhale/main.js
@@ -1,15 +1,33 @@
-const input = "Hi ,I am a whale and I love you"
-const vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+// const input = "Hi ,I am a whale and I love you"
 const resultArray = []
+const vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+const input = document.getElementById("input").value.toLowerCase()
 
-for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < vowel.length; j++) {
-        if (input[i] === vowel[j]) {
-            resultArray.push(vowel[j])
+
+function translateInput(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < vowel.length; j++) {
+            if (input[i] === vowel[j]) {
+                resultArray.push(vowel[j])
+            }
+        }
+        if (input[i] === 'e' || input[i] === 'u') {
+            resultArray.push(input[i])
         }
     }
-    if (input[i] === 'e' || input[i] === 'u') {
-        resultArray.push(input[i])
-    }
+    displayTranslation();
 }
-console.log(resultArray.join('').toUpperCase()) // output: IAAAEEAOEEYOUU
+console.log(resultArray.join('').toUpperCase())
+
+function displayTranslation() {
+
+    let translationBox = document.getElementById("translation-field").style.visibility = "visible";
+    let translationText = document.createElement("p");
+
+    translationText.innerText = resultArray;
+
+    translationBox.appendChild(translationText)
+
+
+
+}
