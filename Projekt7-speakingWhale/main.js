979a3b0e6dc95a8a6e6b99ea93392b054275197e
Christin Bergmann <christin.bergmann@gmx.net>
2020-05-18 11:56:22 +0200

    add spellcheck

--- Projekt7-speakingWhale/main.js
+++ Projekt7-speakingWhale/main.js
@@ -1,33 +1,29 @@
-// const input = "Hi ,I am a whale and I love you"
-const resultArray = []
-const vowel = ['a', 'e', 'i', 'o', 'u', 'y']
-const input = document.getElementById("input").value.toLowerCase()
+function translateInput() {
+    const resultArray = []
+    const vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+    const input = document.getElementById("input").value.toLowerCase()
 
-
-function translateInput(input) {
     for (let i = 0; i < input.length; i++) {
         for (let j = 0; j < vowel.length; j++) {
             if (input[i] === vowel[j]) {
-                resultArray.push(vowel[j])
+                resultArray.push(vowel[j]);
             }
         }
         if (input[i] === 'e' || input[i] === 'u') {
-            resultArray.push(input[i])
+            resultArray.push(input[i]);
         }
     }
-    displayTranslation();
+    displayTranslation(resultArray.join('').toUpperCase(), input.toUpperCase());
 }
-console.log(resultArray.join('').toUpperCase())
+// console.log(resultArray.join('').toUpperCase())
 
-function displayTranslation() {
+function displayTranslation(resultArray, input) {
 
-    let translationBox = document.getElementById("translation-field").style.visibility = "visible";
-    let translationText = document.createElement("p");
+    document.getElementById("translation").style.visibility = "visible";
 
-    translationText.innerText = resultArray;
-
-    translationBox.appendChild(translationText)
-
-
-
+    let translationText = document.createElement("p")
+    translationText.innerHTML = `${input} : ${` " ${resultArray} " `}`;
+    translationText.style.display = "visible";
+    translation.appendChild(translationText)
+    document.getElementById("input").value = "";
 }
